dded3bb168bc9b0ac0501f8ae5e1fcbe8ec58dd1
Erick Kramer <erickkramer@gmail.com>
2019-04-25 15:28:45 +0200

    Parallel hill climbing search implementation

---
+++ assignments/09/parallelhillclimber.py
@@ -0,0 +1,49 @@
+
+import copy
+import pickle
+from population import POPULATION
+
+'''
+Instructions:
+Implement hill climber search
+www.reddit.com/r/ludobots/wiki/pyrosim/parallelhillclimber
+
+Blue diamond is the y coordinate
+'''
+
+save_best = True
+visualize_best = True
+num_generations = 300
+
+# Initial population
+parents = POPULATION(pop_size=100)
+parents.create_population()
+parents.evaluate()
+
+print('Generation 0...')
+parents.print_pop()
+
+for gen in range(num_generations):
+    children = copy.deepcopy(parents)
+    children.mutate()
+    children.evaluate()
+
+    parents.replace(children)
+    print('Generation ', gen+1, '...')
+    parents.print_pop()
+
+fittest_idx = parents.find_fittest()
+
+print('-----------------------------------------')
+print('The best individual found after {} generations is individual {} with a fitness of {}'\
+      .format(num_generations, fittest_idx, parents.p[fittest_idx].fitness))
+print('-----------------------------------------')
+
+if visualize_best:
+    parents.p[fittest_idx].start_evaluation(blind_mode=False)
+    parents.p[fittest_idx].compute_fitness()
+
+if save_best:
+    f = open('robot.p', 'wb')
+    pickle.dump(parents.p[fittest_idx], f)
+    f.close()
